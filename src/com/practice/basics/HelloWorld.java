73b1bbf1b8e9347f10d3f4b949000f04545f91b2
shankarallada <shankarallada123@gmail.com>
2023-08-16 22:07:26 +0530

    example one

--- src/com/practice/basics/HelloWorld.java
+++ src/com/practice/basics/HelloWorld.java
@@ -2,7 +2,7 @@ package com.practice.basics;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String[]args) {
 	System.out.println("hello world shankar");	
 	}
 }
