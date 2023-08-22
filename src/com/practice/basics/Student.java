204a4a27c8f6bca99d667ae37518b29e960aa017
shankarallada <shankarallada123@gmail.com>
2023-08-22 09:32:35 +0530

    Actor,Student,Doctor

---
+++ src/com/practice/basics/Student.java
@@ -0,0 +1,30 @@
+package com.practice.basics;
+
+public class Student {
+	public static void main(String[] args) {
+		
+		//variables declaration
+		 int rollno = 404 ;
+		 String name = "Allada shankar";
+		 String department = "ECE";
+		 String roomno = "g7";
+		 String gender = "male";
+		 int age = 21;
+	     String address = "munagapaka,anakapalli, 531033";
+	     long phone = 8765543321l ;
+	     
+	     //logic 
+	    System.out.println("Name: "+name);
+	    System.out.println("Age :"+age);
+	    System.out.println("Rollno: "+rollno);
+	    System.out.println("Gender: "+gender);
+	    System.out.println("Phone: "+phone);
+	    System.out.println("Roomno: "+roomno);
+	    System.out.println("Department: "+department);
+	    System.out.println("Address: "+address);
+	    
+		 
+		 }
+	
+
+}
