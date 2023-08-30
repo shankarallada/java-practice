500b77a80522d612ff51a455ea641ba1a28ab767
shankarallada <shankarallada123@gmail.com>
2023-08-30 08:24:26 +0530

    logicaloperators

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,20 @@
+package com.practice.operators;
+
+public class ArithmeticOperators {
+	public static void main(String[] args) {
+		int a = 15;
+		int b = 3;
+		
+		int add = a+b; //18
+		System.out.println("Sum :"+add);
+		int sub = a-b; //15
+		System.out.println("sub :"+sub);
+		int mul = a*b; //45
+		System.out.println("Mul :"+mul);
+		int  div = a/b; //	15
+		System.out.println("Div :"+div);
+		int mod = a%b;
+		System.out.println("Mod :"+mod);
+	}
+
+}
---
+++ src/com/practice/operators/src/com/practice/basics/ExampleThree.java
@@ -0,0 +1,9 @@
+package com.practice.basics;
+
+public class ExampleThree {
+	public static void main(String[] args) {
+		System.out.println("my example three");
+		
+	}
+
+}
