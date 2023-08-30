917d50f117d93c15e555ae38273016ba3598f631
shankarallada <shankarallada123@gmail.com>
2023-08-30 08:15:16 +0530

    logical

---
+++ com/practice/basics/Doctor.java
@@ -0,0 +1,19 @@
+package com.practice.basics;
+
+public class Doctor {
+	public static void main(String[] args) {
+		String name = "Dr.mukesh";
+		String specialization = "ophthalmologist";
+		int salary = 40000;
+		String address = "munagapaka,anakapalli,531033";
+		String hospitalname = "munagapaka government hospital";
+		
+		System.out.println("Name: "+name);
+		System.out.println("Salary: "+salary);
+		System.out.println("Specialization: "+specialization);
+		System.out.println("Hospitalname: "+hospitalname);
+		System.out.println("address: "+address);
+		
+	}
+
+}
